68f601a421f203f8014a906874025e46ecee36fc
Bruno Ferreira Filipini <brunofilipini04@gmail.com>
2024-10-07 15:59:16 -0300

    Update test.py

--- PDF-AI/test.py
+++ PDF-AI/test.py
@@ -2,7 +2,7 @@ import requests
 
 response = requests.get(
     'https://api-inference.huggingface.co/models/google/flan-t5-xxl',
-    headers={'Authorization': f'Bearer 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'}
+    headers={'Authorization': f'Bearer YOUR_KEY'}
 )
 
 print(response.status_code)
